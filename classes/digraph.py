3b880aee7cb3dc29e4e2759bf9b0003c40a853ac
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2023-09-25 23:44:29 +0300

    Update digraph.py

--- classes/digraph.py
+++ classes/digraph.py
@@ -28,6 +28,7 @@ class DiGraph():
     def intersection(self, second_digraph):
         #TODO Добавить валидацию
         new_digraph = nx.DiGraph()
+        new_digraph.add_nodes_from(self.digraph.nodes)
         new_digraph.add_edges_from(set(self.digraph.edges).intersection(set(second_digraph.edges)))
         return new_digraph
     
@@ -35,6 +36,7 @@ class DiGraph():
     def union(self, second_digraph):
         #TODO Добавить валидацию
         new_digraph = nx.DiGraph()
+        new_digraph.add_nodes_from(self.digraph.nodes)
         new_digraph.add_edges_from(set(self.digraph.edges).union(set(second_digraph.edges)))
         return new_digraph
 
@@ -42,6 +44,7 @@ class DiGraph():
     def difference(self, second_digraph):
         #TODO Добавить валидацию
         new_digraph = nx.DiGraph()
+        new_digraph.add_nodes_from(self.digraph.nodes)
         new_digraph.add_edges_from(set(self.digraph.edges).difference(set(second_digraph.edges)))
         return new_digraph
 
