7d2cae5ec50f35a57e48134170e69dbbd77d20b2
Hubbabubba <pipusic@gmail.com>
2023-09-25 20:14:12 +0300

    Добавлена операция пересечения в класс разрез

--- classes/section.py
+++ classes/section.py
@@ -20,11 +20,18 @@ class Section():
     def narrowing(self, set):
         #TODO Добавить валидацию
         pass
-    
 
     def intersection(self, second_section):
-        #TODO Добавить валидацию
-        pass
+        intersected = {}
+        for key in self.section:
+            intersected[key] = {'R+': [], 'R-': []}
+            for node in self.section[key]['R+']:
+                if node in second_section[key]['R+']:
+                    intersected[key]['R+'].append(node)
+            for node in self.section[key]['R-']:
+                if node in second_section[key]['R-']:
+                    intersected[key]['R-'].append(node)
+        return intersected
     
 
     def union(self, second_section):
@@ -32,14 +39,8 @@ class Section():
         pass
 
     def difference(self, second_section):
-        for key in second_section:
-            for node in second_section[key]['R+']:
-                if node in self.section[key]['R+']:
-                    self.section[key]['R+'].remove(node)
-            for node in second_section[key]['R-']:
-                if node in self.section[key]['R-']:
-                    self.section[key]['R-'].remove(node)
-        return self.section
+        #TODO Добавить валидацию
+        pass
     
 
     def symmetric_difference(self, second_section):
